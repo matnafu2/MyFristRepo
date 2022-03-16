983a4b1fdb5ec1ff0eff56772922ca2c883fdc98
matnafu2 <micomike30@gmail.com>
2022-03-15 23:27:34 -0400

    add test to sub and add method and modify main.yml

---
+++ TestSub.java
@@ -0,0 +1,8 @@
+import junit.framework.*
+
+public class TestSub extends TestCase {
+	public void testSub() {
+		int res = Calculator.sub(2,3);
+		assertTrue(res == -1)
+	}
+}
--- test.java
+++ test.java
@@ -4,5 +4,9 @@ class Calclator {
 
 	public static int add(int num1, int num2) {
 		return num1 + num2;
-	} 
+	}
+
+	public static int sub(int num1, int num2) {
+		return num1 + num2;
+	}  
 }
